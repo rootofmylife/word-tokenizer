ed5fbb322d49b51e553e62198849334ef7d37a41
Duc Do <dotrananhduc@gmail.com>
2017-12-15 16:19:48 +0700

    file uploaded

---
+++ word_segment.py
@@ -0,0 +1,69 @@
+from pprint import pprint
+import sys
+
+if __name__ == '__main__':
+	with open("./VDic.txt") as f:
+		file_dict = f.read().splitlines()
+	with open("./corpus.txt") as fc:
+		corpus = fc.read().splitlines()
+
+	sym = r"~`!@#$%^&*()_-+=[]{}|;':\"”“,./<>?–"
+	corpus = [s.translate({ord(c): "" for c in sym}) for s in corpus]	
+	corpus = [s.lower() for s in corpus]
+	#key_search = sys.argv[1]
+	file_dict.remove(file_dict[0])
+	file_dict = [x.split('\t') for x in file_dict]
+	vdic = []
+	for i in range(len(file_dict) - 1):
+		vdic.append(file_dict[i][0])
+	corpus = [s.split() for s in corpus]
+	corpus = corpus[0]
+	
+	'''
+	if key_search in vdic:
+		print('true')
+	else:
+		print('false')
+	'''
+	output = open("corpus_out.txt", "w")
+	temp = ""
+	'''
+	check = 1
+	for j in range(len(corpus)):
+		if check == 1:
+			temp += corpus[j]
+		elif check == 2:
+			temp += " " + corpus[j]
+		if temp in vdic:
+			output.write(temp.replace(' ', '_') + " ")
+			temp = ""
+			check = 1
+		else:
+			if temp + " " + corpus[j + 1] in vdic:
+				check = 2
+			else:
+				output.write(temp + " ")
+				temp = ""
+				check = 1
+	'''
+	vt = -1
+	for j in range(len(corpus)):
+		if j <= vt:
+			continue
+		temp = corpus[j]
+		index = j + 1
+		check = 0
+		while(index < j + 3):
+			if index >= len(corpus) - 1:
+				break
+			temp2 = temp + " " + corpus[index]
+			if temp2 in vdic:
+				vt = index
+				check = 1
+				output.write(temp2.replace(' ', '_') + " ")
+				break
+			index = index + 1
+		if check == 0:
+			output.write(temp + " ")	
+	output.close()
+			
